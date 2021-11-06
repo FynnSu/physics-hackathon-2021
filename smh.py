a1f7552ab3b308df131a128ce488231dda0cb790
luca-weishaupt <luca.weishaupt@mail.mcgill.ca>
2021-11-06 14:54:07 -0400

    added fancy gif plotting stuff

--- smh.py
+++ smh.py
@@ -1,9 +1,13 @@
 import numpy as np
 from scipy.integrate import odeint
+from matplotlib import pyplot as plt
+import imageio
+import glob
 
-
-def apply_gravity(y,vy):
-    pass
+def apply_gravity(theta, t, g, m, k):
+    y1, vy1 = theta
+    dthetadt = [vy1, -m*g-y1*k]
+    return dthetadt
 
 
 def apply_physics(positions, velocities, density, dt):
@@ -12,6 +16,29 @@ def apply_physics(positions, velocities, density, dt):
 
 def main():
     print("Hello World")
+    g = 10
+    m = 1
+    k = 1
+    t = np.linspace(0,5,20)
+    theta0 = (-5,0)
+
+    sol = odeint(apply_gravity, theta0, t, args=(g, m, k))
+    fig, ax = plt.subplots()
+    ax.plot(t, sol[:, 0], label="position1")
+    plt.legend()
+    plt.show()
+
+    images = []
+    ymin, ymax = np.min(sol[:,0]), np.max(sol[:,0])
+    for i in range(len(sol)):
+        fig, ax = plt.subplots()
+        plt.ylim(ymin-1, ymax+1)
+        ax.scatter(0, sol[i][0], label="position1")
+        plt.legend()
+        fname = f"tmp/tmp.png"
+        plt.savefig(fname)
+        images.append(imageio.imread(fname))
+    imageio.mimsave('movie.gif', images)
 
 
 if __name__ == '__main__':
