b9775c84d8ea77a2c651212e8c25b68dafa3b092
luca-weishaupt <luca.weishaupt@mail.mcgill.ca>
2021-11-06 15:14:36 -0400

    added fancy gif plotting stuff

--- smh.py
+++ smh.py
@@ -2,7 +2,21 @@ import numpy as np
 from scipy.integrate import odeint
 from matplotlib import pyplot as plt
 import imageio
-import glob
+
+
+def make_gif(positionFrames, outPath='movie.gif'):
+    images = []
+    ymin, ymax = np.min(positionFrames), np.max(positionFrames)
+    for i in range(len(positionFrames)):
+        fig, ax = plt.subplots()
+        plt.ylim(ymin-1, ymax+1)
+        ax.plot(range(len(positionFrames[0])), positionFrames[i])
+        fname = f"tmp/tmp.png"
+        plt.savefig(fname)
+        images.append(imageio.imread(fname))
+    imageio.mimsave(outPath, images)
+
+
 
 def apply_gravity(theta, t, g, m, k):
     y1, vy1 = theta
@@ -28,17 +42,21 @@ def main():
     plt.legend()
     plt.show()
 
-    images = []
-    ymin, ymax = np.min(sol[:,0]), np.max(sol[:,0])
-    for i in range(len(sol)):
-        fig, ax = plt.subplots()
-        plt.ylim(ymin-1, ymax+1)
-        ax.scatter(0, sol[i][0], label="position1")
-        plt.legend()
-        fname = f"tmp/tmp.png"
-        plt.savefig(fname)
-        images.append(imageio.imread(fname))
-    imageio.mimsave('movie.gif', images)
+
+    madeuppositions = np.random.uniform(-1,1,[20,30])
+    make_gif(madeuppositions)
+
+    # images = []
+    # ymin, ymax = np.min(sol[:,0]), np.max(sol[:,0])
+    # for i in range(len(sol)):
+    #     fig, ax = plt.subplots()
+    #     plt.ylim(ymin-1, ymax+1)
+    #     ax.scatter(0, sol[i][0], label="position1")
+    #     plt.legend()
+    #     fname = f"tmp/tmp.png"
+    #     plt.savefig(fname)
+    #     images.append(imageio.imread(fname))
+    # imageio.mimsave('movie.gif', images)
 
 
 if __name__ == '__main__':
