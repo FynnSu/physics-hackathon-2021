5d28a39d9c0f482959890ab3e5ac75f476369e00
cmdenis <christianmdenis@gmail.com>
2021-11-06 14:02:31 -0400

    updated fluc_sim again :)

--- fluc_sim.py
+++ fluc_sim.py
@@ -2,8 +2,6 @@ import numpy as np
 
 test_a = np.array([1, 2, 3, 4])
 
-print(np.roll(test_a, 1))
-
 def functional(sites):
     '''Functional that computes the energy for a given function.'''
     return np.sum((sites - np.roll(sites))**2)
